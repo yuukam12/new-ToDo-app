40d2108568fa4a327dd571d9f10f9d92b7db22e5
yuukam12 <chococookie147@gmail.com>
2023-08-17 11:34:02 +0900

    newtodoapp

---
+++ ToDo/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+
+class TodoItem(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
