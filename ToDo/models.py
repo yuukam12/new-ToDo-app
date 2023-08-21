a23b56b0f1e5526ef713e3103ed46b90bac63e79
yuukam12 <chococookie147@gmail.com>
2023-08-21 16:09:29 +0900

    create view import

--- ToDo/models.py
+++ ToDo/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 
-class TodoItem(models.Model):
+class ToDoItem(models.Model):
     id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=100)
     description = models.TextField()
--- ToDo/views.py
+++ ToDo/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from .models import ToDoItem
+from .serializers import ToDoItemSerializer
 # Create your views here.
 # create view
+
+
+class TodoItemViewSet(viewsets.ModelViewSet):
+    queryset = ToDoItem.objects.all()
+    serializer_class = ToDoItemSerializer
+
