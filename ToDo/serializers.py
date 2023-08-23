9618a8ef9ed568d7aa2df22c2e884a79663bcdbb
yuukam12 <chococookie147@gmail.com>
2023-08-24 04:07:34 +0900

    check api

---
+++ ToDo/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from .models import ToDoItem
+
+
+class ToDoItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ToDoItem
+        fields = ('title', 'description', 'created_at')
+        # 各種ID情報など、内部でのみ使用されるようなデータもシリアライズしない方が良いことが多い？（ChatGPT)
---
+++ ToDo/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
+from .models import ToDoItem
+from .serializers import ToDoItemSerializer
+from django.db.models import Q
+# Create your views here.
+# create view
+
+
+class ToDoItemViewSet(viewsets.ModelViewSet):
+    queryset = ToDoItem.objects.all()
+    serializer_class = ToDoItemSerializer
+    permission_classes = [AllowAny]
+
+    def filter_queryset(self, queryset):  # titleもしくはdescriptionと入力したkeywordが部分一致したものだけを表示する
+        keyword = self.request.query_params.get('keyword', None)
+        if keyword:
+            queryset = queryset.filter(Q(title__icontains=keyword) | Q(description__icontains=keyword))
+        return queryset
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
